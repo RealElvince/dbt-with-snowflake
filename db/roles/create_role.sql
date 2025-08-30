93f928eb946aef1bc9a7028c56c66528608497f4
RealElvince <obuongely@yahoo.com>
2025-08-30 13:01:07 +0300

    db role

---
+++ db/roles/create_role.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE ROLE DBT_USER;
+-- Allow usage of database and schema
+GRANT USAGE ON DATABASE dbt_cloud TO ROLE DBT_USER;
+GRANT USAGE ON SCHEMA dbt_cloud.dbt_schema TO ROLE DBT_USER;
+
+-- Allow creating and modifying tables in the schema
+GRANT CREATE TABLE ON SCHEMA dbt_cloud.dbt_schema TO ROLE DBT_USER;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA dbt_cloud.dbt_schema TO ROLE DBT_USER;
